e537c43cf0663a9e954e42ece233e774ac94c5a3
Lucas Rodrigues <lucasrodrigues.oliveira0809@gmail.com>
2025-09-24 23:46:31 -0300

    Finalização

---
+++ Main.py
@@ -0,0 +1,35 @@
+palavra_secreta = 'toalha.'
+letras_acertadas = ['_'] * len(palavra_secreta)
+tentativas = 9 
+
+print ('Bem vindo ao jogo da forca! ')
+
+palavra_mostrada = ' '
+
+while tentativas > 0 : 
+
+    print('Adivinhe a palavra: ' " ".join(letras_acertadas))
+    print (f'Você ainda tem {tentativas} tentativas. ')
+
+    chute = input('Qual letra deseja chutar ? ').lower()
+
+    if chute in palavra_secreta :  
+        print ('Você acertou! ')
+
+        for indice, letra in enumerate(palavra_secreta) : 
+            if chute == letra : 
+                letras_acertadas[indice] = letra
+
+    else : 
+        tentativas -= 1 
+        print ('Você errou! Tente novamente! ')
+        print (f'Você ainda tem {tentativas} tentativas. ')
+
+    if "_" not in letras_acertadas : 
+        print (f'Você ganhou! A palavra era {palavra_secreta}  ')
+        break
+
+    
+
+        
+        
